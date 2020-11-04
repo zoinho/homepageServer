8876f848702a9f206419d6785386d55b8270e89d
LukaszM <lukasz.milik@goprama.com>
2020-11-04 12:12:10 +0100

    vercel files

--- api/index.js
+++ api/index.js
@@ -9,6 +9,8 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header('Cache-Control', 's-max-age=1, stale-while-revalidate')
+
     next();
   });
 app.use(express.json())
